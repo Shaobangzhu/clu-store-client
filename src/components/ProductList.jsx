f674d0330a38e8845890d72a30202b950543271f
Chaoran Lu <andyluloveusc@gmail.com>
2026-05-11 23:01:15 -0700

    feat: add lesson 4-7 reusable component composition pattern

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,14 +2,42 @@
 import React from "react";
 import Product from "@components/Product";
 
+const ReleaseNote = () => (
+  <div>
+    <h1>新产品发布日期: 2199-01-01</h1>
+    <h2>请耐心等待</h2>
+  </div>
+);
+
+const ProductNotFound = () => (
+  <div>
+    <h1>产品未发布</h1>
+    <h2>请耐心等待</h2>
+  </div>
+);
+
+const ListTitle = () => (
+  <div style={{ display: "flex", justifyContent: "center" }}>
+    <h1
+      style={{
+        fontWeight: "800",
+        backgroundImage: "url('src/assets/lines.png')",
+        backgroundPosition: "center",
+      }}
+    >
+      上新品，各个添新意
+    </h1>
+  </div>
+);
+
 function ProductList({ data }) {
   const isReleased = new Date() <= new Date("2199-01-01T00:00:00");
   if (!isReleased) {
-    return <div>新产品发布日期: 2199-01-01, 请耐心等待</div>;
+    return <ReleaseNote />;
   }
 
   if (data.length <= 0) {
-    return <div>产品未发布</div>;
+    return <ProductNotFound />;
   }
   return (
     <div
@@ -19,18 +47,7 @@ function ProductList({ data }) {
         rowGap: "3rem",
       }}
     >
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <h1
-          style={{
-            fontWeight: "800",
-            backgroundImage: "url('src/assets/lines.png')",
-            backgroundPosition: "center",
-          }}
-        >
-          上新品，各个添新意
-        </h1>
-      </div>
-
+      <ListTitle />
       {data.map((p) => (
         <Product {...p} key={p.title} />
       ))}
