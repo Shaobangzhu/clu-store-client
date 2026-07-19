97f78727d638955c3a52429592922e12dd96b882
Chaoran Lu <andyluloveusc@gmail.com>
2026-07-18 23:03:16 -0700

    fix: grey -> gray

--- src/components/DarkToggle.jsx
+++ src/components/DarkToggle.jsx
@@ -7,8 +7,8 @@ function DarkToggle() {
 
     return (
       <button 
-        className="p-1 bg-gray-200 dark:bg-grey-600 rounded-full
-        hover:bg-grey-300 dark:hover:bg-grey-400 transition-all duration-300
+        className="p-1 bg-gray-200 dark:bg-gray-600 rounded-full
+        hover:bg-gray-300 dark:hover:bg-gray-400 transition-all duration-300
         hover:rotate-12
       "
       onClick={toggleDark}
