c22c0555efbbb72fcbe03d27e21267b336992de3
Chaoran Lu <andyluloveusc@gmail.com>
2026-07-31 11:12:31 -0700

    refactor: migrate React project from JavaScript to TypeScript

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ function App() {
 
 // JSX 是 JavaScript 的语法扩展，允许我们在 JavaScript 代码中编写类似 HTML 的代码.
 // JSX = HTML语法 + JavaScript的能力
-const root = document.getElementById("root"); // 获取id为root的DOM元素
+const root = document.getElementById("root")!; // 获取id为root的DOM元素
 const rootElement = ReactDOM.createRoot(root); // 创建一个 React 根元素
 rootElement.render(
   <StrictMode>
